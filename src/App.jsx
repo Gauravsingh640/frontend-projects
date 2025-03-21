9175dc9270e4da1cba90dea1c38989c1b73cf9d0
Gaurav <gauravsingh71205@gmail.com>
2025-03-22 00:56:38 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { FlashCardContainer } from "./components/FlashCardContainer";
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Flash Cards: JavaScript</h1>
+        <FlashCardContainer />
+      </main>
+      <footer>Made by Gaurav</footer>
+    </>
+  )
+}
+
+export default App
